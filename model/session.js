8dd35c72f700568b7c3e12abeaffa2d8daf7ff51
bobCumulus <39416921+Robert-Doe@users.noreply.github.com>
2021-07-26 13:55:34 +0000

    Update session.js

--- model/session.js
+++ model/session.js
@@ -4,7 +4,7 @@ const sessionSchema = new mongoose.Schema({
     pair_id: String,
     hall_id: String,
     period: String,
-    batch: String
+    batch_id: String
 })
 
 module.exports = new mongoose.model('Sessions', sessionSchema)
