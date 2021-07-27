074d2f56f91b687b1336b2e93558338cd1280828
bobCumulus <39416921+Robert-Doe@users.noreply.github.com>
2021-07-27 02:26:33 +0000

    Update session.js

--- routes/session.js
+++ routes/session.js
@@ -124,6 +124,20 @@ const possibleSet = (duration, maxPeriod) => {
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
+     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next()
 })
 
