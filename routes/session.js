46be587a795db354ba1be1d66563b1a0aa67c1af
bobCumulus <39416921+Robert-Doe@users.noreply.github.com>
2021-07-27 03:17:45 +0000

    Update session.js

--- routes/session.js
+++ routes/session.js
@@ -5,6 +5,26 @@ const Session = require('../model/session');
 
 
 
+// middleware that is specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now())
+     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next()
+})
+
 
 let days = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri'];
 let schedules = [];
@@ -104,42 +124,6 @@ const possibleSet = (duration, maxPeriod) => {
     });
     return freePeriod;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now())
-     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', null);
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next()
-})
 
 
 /*Retrieving all Sessions*/
