6e136a5737470965628d8faf5fba9db85d3d71b0
bobCumulus <39416921+Robert-Doe@users.noreply.github.com>
2021-07-27 22:17:03 +0000

    Update session.js

--- routes/session.js
+++ routes/session.js
@@ -9,17 +9,17 @@ const Session = require('../model/session');
 router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
      // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    //res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
     next()
